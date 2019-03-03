d7e99019652571ca8f8c5c0b0eb4b28e2528243f
chineseluo <848257135@qq.com>
2019-03-03 22:55:01 +0800

    commit

--- seleniumTest1/case/test_login.py
+++ seleniumTest1/case/test_login.py
@@ -14,21 +14,29 @@ class login_EmailTest(unittest.TestCase):
 
     def setUp(self):
         print("用例执行前执行一次")
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://mail.qq.com/cgi-bin/loginpage")
+        time.sleep(2)
 
     def tearDown(self):
         print("用例执行后执行一次")
+
+    def is_login_success(self):
+        try:
+            login_title = self.driver.title
+            return login_title
+        except:
+            return " "
+
     def test_loginQQEmail(self):
-        driver = webdriver.Chrome()
-        driver.get("https://mail.qq.com/cgi-bin/loginpage")
+        self.driver.switch_to.frame(self.driver.find_element_by_id("login_frame"))
+        self.driver.find_element_by_id("u").send_keys("848257135")
+        self.driver.find_element_by_id("p").send_keys("luo@17810538900")
+        self.driver.find_element_by_id("login_button").click()
         time.sleep(2)
-        driver.switch_to.frame(driver.find_element_by_id("login_frame"))
-        driver.find_element_by_id("u").send_keys("###########")
-        driver.find_element_by_id("p").send_keys("###########")
-        driver.find_element_by_id("login_button").click()
-        time.sleep(2)
-        login_title=driver.title
-        driver.switch_to.frame(driver.find_element_by_id("mainFrame"))
-        driver.find_element_by_id("today_alias").click()
+        login_title=self.is_login_success()
+        self.driver.switch_to.frame(self.driver.find_element_by_id("mainFrame"))
+        self.driver.find_element_by_id("today_alias").click()
         print(login_title)
         self.assertEqual("QQ邮箱",login_title)
 
